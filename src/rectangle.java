9e7c6a6d338f266ca7a1af346901e22bff90b446
mac <0x3f3f3f3f3f3f@gmail.com>
2024-10-11 19:54:25 +0800

    df

---
+++ src/rectangle.java
@@ -0,0 +1,14 @@
+public class rectangle {
+    public int height ;
+    public int width;
+
+    public rectangle(int width,int height) {
+        this.width = width;
+        this.height=height;
+    }
+
+    public rectangle() {
+        this(10,10);
+    }
+
+}
